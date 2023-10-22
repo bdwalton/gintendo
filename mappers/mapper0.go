8a8eff540a0e11f711dc9456e35d5a12e36ca5ae
Ben Walton <bdwalton@gmail.com>
2023-10-22 16:48:03 +0100

    Mappers/Mapper0: Add RAM at 0x6000-0x7FFF
    Not yet tested or verified, but ensure we have the writable area that
    cartridges expect.

--- mappers/mapper0.go
+++ mappers/mapper0.go
@@ -7,10 +7,14 @@ func init() {
 
 type mapper0 struct {
 	*baseMapper
+	prgRAM []uint8
 }
 
 func newMapper0() *mapper0 {
-	return &mapper0{baseMapper: newBaseMapper(0, "NROM")}
+	return &mapper0{
+		baseMapper: newBaseMapper(0, "NROM"),
+		prgRAM:     make([]uint8, 0x7FFF-0x6000),
+	}
 }
 
 func (m *mapper0) MemWrite(addr uint16, val uint8) {
@@ -18,8 +22,14 @@ func (m *mapper0) MemWrite(addr uint16, val uint8) {
 	case addr < NES_BASE_MEMORY:
 		m.baseRAM[addr] = val
 	case 0x0800 <= addr && addr <= 0x1FFF:
+		// TODO: Maybe just make all of these mod
+		// NES_BASE_MEMORY so there is 1 case instead of 2
+		// here?  3 banks of $0800 addresses mapped back into
 		// 3 banks of $0800 addresses mapped back into base memory
 		m.baseRAM[(addr-0x0800)%NES_BASE_MEMORY] = val
+	case 0x6000 <= addr && addr <= 0x7FFF:
+		// PRG RAM
+		m.prgRAM[addr-0x6000] = val
 	case 0x8000 <= addr && addr <= 0xFFFF:
 		// If we have two blocks of PRG, we can read higher
 		// within the block, up to 32k. Otherwise, we map the
@@ -39,8 +49,14 @@ func (m mapper0) MemRead(addr uint16) uint8 {
 	case addr < NES_BASE_MEMORY:
 		return m.baseRAM[addr]
 	case 0x0800 <= addr && addr <= 0x1FFF:
-		// 3 banks of $0800 addresses mapped back into base memory
+		// TODO: Maybe just make all of these mod
+		// NES_BASE_MEMORY so there is 1 case instead of 2
+		// here?  3 banks of $0800 addresses mapped back into
+		// base memory
 		return m.baseRAM[(addr-0x0800)%NES_BASE_MEMORY]
+	case 0x6000 <= addr && addr <= 0x7FFF:
+		// PRG RAM
+		return m.prgRAM[addr-0x6000]
 	case 0x8000 <= addr && addr <= 0xFFFF:
 		// If we have two blocks of PRG, we can read higher
 		// within the block, up to 32k. Otherwise, we map the
