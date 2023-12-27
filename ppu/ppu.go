6b0ce03b4cec0c7c1130ae39f8a03461e126103f
Ben Walton <bdwalton@gmail.com>
2023-12-27 10:36:28 +0000

    Add some constants for the PPU status register flags

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -61,6 +61,32 @@ const (
 	CTRL_INCR_DOWN   = 32
 )
 
+// 7  bit  0
+// ---- ----
+// VSO. ....
+// |||| ||||
+// |||+-++++- PPU open bus. Returns stale PPU bus contents.
+// ||+------- Sprite overflow. The intent was for this flag to be set
+// ||         whenever more than eight sprites appear on a scanline, but a
+// ||         hardware bug causes the actual behavior to be more complicated
+// ||         and generate false positives as well as false negatives; see
+// ||         PPU sprite evaluation. This flag is set during sprite
+// ||         evaluation and cleared at dot 1 (the second dot) of the
+// ||         pre-render line.
+// |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+// |          a nonzero background pixel; cleared at dot 1 of the pre-render
+// |          line.  Used for raster timing.
+// +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+//
+//	Set at dot 1 of line 241 (the line *after* the post-render
+//	line); cleared after reading $2002 and at dot 1 of the
+//	pre-render line.
+const (
+	STATUS_SPRITE_OVERFLOW = 1 << 5
+	STATUS_SPRITE_0_HIT    = 1 << 6
+	STATUS_VERTICAL_BLANK  = 1 << 7
+)
+
 type Bus interface {
 	ChrRead(uint16, uint16) []uint8
 	TriggerNMI()
